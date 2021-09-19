3662360a8c4bc3a08c8d5238d5994ddcda017ec7
Matthias Georgi <matti.georgi@gmail.com>
2021-09-20 01:17:53 +0200

    run all segements in one epoch

--- src/datamodules/datasets/track_dataset.py
+++ src/datamodules/datasets/track_dataset.py
@@ -1,26 +1,24 @@
 from typing import List
 from torch.utils.data import Dataset
-import random
 import numpy as np
 import torch
-from ...tracks import Track
+from ...tracks import Track, Segment
 
 class TrackDataset(Dataset):
-    tracks: List[Track]
+    segments: List[Segment]
     seq_len: int
 
     def __init__(self, tracks: List[Track], seq_len: int):
         super().__init__()
-        self.tracks = tracks
+        self.segments = [s for t in tracks for s in t.segments]
         self.seq_len = seq_len
 
     def __len__(self):
-        return len(self.tracks)
+        return len(self.segments)
 
     def __getitem__(self, index: int):
-        track = self.tracks[index]
-        i = random.randint(0, len(track.segments) - 1)
-        x = track.segments[i].spec.T[:self.seq_len]
+        seg = self.segments[index]
+        x = seg.spec.T[:self.seq_len]
         if x.shape[0] < self.seq_len:
             x = np.pad(x, [(0, self.seq_len - x.shape[0]), (0, 0)], mode='linear_ramp')
         return torch.tensor(x).unsqueeze(0)
